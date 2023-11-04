983bbd784d5e84dfe29bdfb197fa84dd55ddb4e1
Pedro Alejandro Pacheco Tripi <pedro.pacheco@softtek.com>
2023-11-04 21:19:46 +0100

    Manage PathArguments & Comments for Local Testing

--- lambda/src/server.ts
+++ lambda/src/server.ts
@@ -1,11 +1,14 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import controllers from "./routes/index";
+import { extractParameters } from "./utils";
 
-exports.handler = async function(event: any) : Promise<any> {
+exports.handler = async function(event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> {
     let response;
     try {
-        const controller = controllers[event.path];
-        if (controller)
+        const { parameters, match } = extractParameters(event.path, controllers);
+        if (match)
         {
+            const controller = controllers[match];
             response = await controller.handle(event);
         }
         else {
@@ -20,6 +23,12 @@ exports.handler = async function(event: any) : Promise<any> {
             body: JSON.stringify({ message: 'Internal Server Error ' + error })
         };
     }
-
     return response;
 };
+
+// handler({
+//     path: '/api/v1/softtek', 
+//     httpMethod: 'GET', 
+//     body: '{\n\t"name": "Softtek Las Rozas",\n\t"description": "He is happy now!"\n}', 
+//     queryStringParameters: { softtekId: 2 } as any 
+// } as APIGatewayProxyEvent);
