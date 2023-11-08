6216fd71face67cf3e2e383d7cb60610696aa2cb
Pedro Alejandro Pacheco Tripi <pedro.pacheco@softtek.com>
2023-11-08 16:49:36 +0100

    Update Handler & ApiGateway Payload

--- infra/components/poc/poc.tf
+++ infra/components/poc/poc.tf
@@ -11,7 +11,7 @@ module "lambda_poc" {
 module "lambda_net_poc" {
   source  = "../../modules/lambda"
   name    = "poc-net-rest-api"
-  handler = "TodoApi2"
+  handler = "WebApplication1"
   runtime = "dotnet6"
 }
 
@@ -46,7 +46,7 @@ module "cloudfront_poc" {
       allowed_methods             = null
       cache_policy_id             = data.aws_cloudfront_cache_policy.cloudfront_cache_policy_managed_caching_disabled.id
       origin_request_policy_id    = null
-      path_pattern                = "/api/todoitems"
+      path_pattern                = "/todo"
       target_origin_id            = module.api_gateway_net_poc.id
       lambda_function_association = []
       function_association        = []
--- infra/modules/api_gateway/api_gateway.tf
+++ infra/modules/api_gateway/api_gateway.tf
@@ -4,21 +4,22 @@ resource "aws_apigatewayv2_api" "api_gateway" {
 }
 
 resource "aws_apigatewayv2_integration" "api_gateway_integration" {
-  api_id           = aws_apigatewayv2_api.api_gateway.id
-  integration_type = "AWS_PROXY"
-  integration_method        = "POST"
-  integration_uri           = var.lambda.invoke_arn
+  api_id                 = aws_apigatewayv2_api.api_gateway.id
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
+  integration_uri        = var.lambda.invoke_arn
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "api_gateway_route" {
   api_id    = aws_apigatewayv2_api.api_gateway.id
   route_key = "$default"
-  target = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
 }
 
 resource "aws_apigatewayv2_stage" "api_gateway_stage" {
-  api_id = aws_apigatewayv2_api.api_gateway.id
-  name   = "$default"
+  api_id      = aws_apigatewayv2_api.api_gateway.id
+  name        = "$default"
   auto_deploy = true
 }
 
