78fbaaa360ef2e09e9cd21f764f3561d81b0fc9c
Pedro Alejandro Pacheco Tripi <pedro.alejandro.pt@gmail.com>
2024-01-10 22:31:20 +0100

    Update CF

--- infra/components/poc/poc.tf
+++ infra/components/poc/poc.tf
@@ -31,6 +31,13 @@ module "api_gateway_poc" {
   tags   = { env = "softtek-poc" }
 }
 
+module "api_gateway_poc_express" {
+  source = "../../modules/api_gateway"
+  name   = "poc-rest-api"
+  lambda = module.lambda_express_poc
+  tags   = { env = "softtek-poc" }
+}
+
 module "api_gateway_net_poc" {
   source = "../../modules/api_gateway"
   name   = "poc-net-rest-api"
@@ -42,13 +49,13 @@ module "cloudfront_poc" {
   source         = "../../modules/cloudfront"
   region         = "us-east-1"
   description    = "POC API Rest"
-  origins_custom = [module.api_gateway_poc, module.api_gateway_net_poc]
+  origins_custom = [module.api_gateway_poc, module.api_gateway_poc_express, module.api_gateway_net_poc]
   tags           = { env = "softtek-poc" }
 
   default_cache_behavior = {
     cache_policy_id             = data.aws_cloudfront_cache_policy.cloudfront_cache_policy_managed_caching_disabled.id
     origin_request_policy_id    = null
-    target_origin_id            = module.api_gateway_poc.id
+    target_origin_id            = module.api_gateway_poc_express.id
     lambda_function_association = []
     function_association        = []
   }
